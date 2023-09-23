a57c3eecde710237cb0f26e3723941ce8eb7a53c
dineshwar19 <101964219+dineshwar19@users.noreply.github.com>
2023-09-23 16:32:24 +0530

    useFetch custom hook

--- reactJs/reactlearning/src/App.js
+++ reactJs/reactlearning/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import CustomHook from "./CustomHook";
 import ScreenSize from "./ScreenSize";
+import Api_Data from "./customHooks/Api_Data";
 function App() {
-   return <ScreenSize />;
+   return <Api_Data />;
 }
 
 export default App;
