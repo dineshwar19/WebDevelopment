0724de2f683bfad8d255e0b15c02221275462f55
dineshwar19 <101964219+dineshwar19@users.noreply.github.com>
2023-09-23 10:04:02 +0530

    new Changes and custom hooks

--- reactJs/reactlearning/src/App.js
+++ reactJs/reactlearning/src/App.js
@@ -1,13 +1,8 @@
 import React, { useState } from "react";
-import Use_Ref from "./Use_Ref";
-import Use_Memo from "./Use_Memo";
-import Use_CallBack from "./Use_CallBack";
-import Use_Context from "./Use_Context";
-import Use_Reducer from "./Use_Reducer";
-import Use_State from "./Use_state";
-import Use_Reducer2 from "./Use_Reducer2";
+import CustomHook from "./CustomHook";
+import ScreenSize from "./ScreenSize";
 function App() {
-   return <Use_Reducer2 />;
+   return <ScreenSize />;
 }
 
 export default App;
