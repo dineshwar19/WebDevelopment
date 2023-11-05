1fc030008db1602b54a5e581412cff359eb1b641
dineshwar19 <101964219+dineshwar19@users.noreply.github.com>
2023-11-05 11:00:45 +0530

    todo list with api

--- reactJs/tic-tac-toe/src/App.js
+++ reactJs/tic-tac-toe/src/App.js
@@ -1,11 +1,22 @@
-import "./App.css";
-import Board from "./Board";
-function App() {
-  return (
-    <>
-      <Board />
-    </>
-  );
+import {useState} from 'react';
+
+function Child({handleClick}) {
+  return <button onClick={handleClick}>Increment</button>;
 }
 
-export default App;
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick(event, num) {
+    console.log('Function ran in Child component');
+    setCount(count + num);
+  }
+
+  return (
+    <div>
+      <h2>Count is: {count}</h2>
+
+      <Child handleClick={event => handleClick(event, 100)} />
+    </div>
+  );
+}
